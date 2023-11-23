001c0cee15ae704e5a3aaf32965a0062d730d317
Kate Bakhtina <almazniegori@gmail.com>
2023-11-23 14:57:14 +0300

    change_favorites

--- bd/vkinder_bd_models.py
+++ bd/vkinder_bd_models.py
@@ -18,20 +18,20 @@ class Users(Base):
         return f"{self.id}: ({self.user_id}, {self.fullname}, {self.age}, {self.gender}, {self.city})"
 
 
-class Favorites(Base):
-    __tablename__ = "favorites"
+class Viewed(Base):
+    __tablename__ = "viewed"
 
     id = Column(Integer, primary_key=True)
     fullname = Column(String(40), nullable=False)
-    favorite_user_id = Column(Integer, nullable=False)
-    user_id = Column(Integer, ForeignKey(Users.user_id, ondelete="CASCADE"), nullable=False)
+    viewed_user_id = Column(Integer, nullable=False)
+    status = Column(Integer, nullable=False)  # 0 просмотрено, 1 избранное
     photos = Column(String)
+    user_id = Column(Integer, ForeignKey(Users.user_id, ondelete="CASCADE"), nullable=False)
 
     user = relationship(Users, backref=__tablename__)
 
     def __str__(self):
         return f"{self.id}: ({self.favorite_user_id}, {self.fullname}, {self.user_id})"
-
 
 class BlackList(Base):
     __tablename__ = "black_list"
@@ -44,20 +44,6 @@ class BlackList(Base):
 
     def __str__(self):
         return f"{self.id}: ({self.black_user_id}, {self.user_id})"
-
-#
-# class Photos(Base):
-#     __tablename__ = "photos"
-#
-#     photo_id = Column(Integer, primary_key=True)
-#     link = Column(String, nullable=False)
-#     favorite_user_id = Column(Integer, ForeignKey(Favorites.id, ondelete="CASCADE"), nullable=False, unique=True)
-#
-#     favorite_user = relationship(Favorites, backref=__tablename__)
-#
-#     def __str__(self):
-#         return f"{self.photo_id}: ({self.link}, {self.favorite_user_id})"
-
 
 def create_table(engine):
     Base.metadata.drop_all(engine)
